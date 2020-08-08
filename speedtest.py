b33a133eceb2a9e8dedf04818e17da7a5b626057
Brian Craig <brianwilfredcraig@gmail.com>
2020-08-08 11:15:17 +0100

    added ssid to data collected

--- speedtest.py
+++ speedtest.py
@@ -25,6 +25,23 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 logger.addHandler(fh)
 
+logger.info('about to call netsh wlan')
+netsh_output = subprocess.Popen("netsh wlan show interfaces", shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+logger.debug('wifi_ssif=>%s<', netsh_output)
+logger.info('completed call to netsh wlan')
+
+outputArray = netsh_output.splitlines(False)
+logger.debug('outputArray=>%s<', outputArray)
+
+outputArrayNoSpaces = [x.replace(' ','') for x in outputArray]
+logger.debug('outputArrayNoSpaces=>%s<', outputArrayNoSpaces)
+
+ssidArray = [s for s in outputArrayNoSpaces if s.startswith("SSID")]
+logger.debug('ssidArray=>%s<', ssidArray)
+
+wifi_ssid = ssidArray[0]
+logger.info('wifi_ssid=>%s<', wifi_ssid)
+
 logger.info('about to call speedtest-cli')
 response = subprocess.Popen('%PYTHON_HOME%\Scripts\speedtest-cli.exe --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 logger.info('completed call to speedtest-cli')
@@ -41,9 +58,9 @@ upload = upload[0].replace(',', '.')
 try:
     f = open('output_speedtest.csv', 'a+')
     if os.stat('output_speedtest.csv').st_size == 0:
-            f.write('Date,Time,Download,Upload,Ping\r')
+            f.write('SSID,Date,Time,Download,Upload,Ping\r')
 
-    f.write('{},{},{},{},{}\r'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), download, upload, ping))
+    f.write('{},{},{},{},{},{}\r'.format(wifi_ssid, time.strftime('%m/%d/%y'), time.strftime('%H:%M'), download, upload, ping))
 
 except Exception as e:
     logger.info('exception during file write')
